396109a076a1af9289c85f950a4e8a2ea42ac886
NairaBStar <113399059+NairaBStar@users.noreply.github.com>
2023-01-09 15:49:50 -0800

    Update LEDMatrixGame.ino

--- LEDMatrixGame.ino
+++ LEDMatrixGame.ino
@@ -1,4 +1,3 @@
-#include "LedControl.h"
 #include "time.h"
 
 int left = 5;
@@ -7,8 +6,6 @@ int right = 3;
 
 //initializes starting point of player, the variable keeps track of player location in bottom row
 int button_position = 3;
-
-LedControl lc = LedControl(9, 11, 6, 1);
 
 
 void PositionController(){
@@ -19,28 +16,17 @@ void PositionController(){
   //Read buttons and act on it
   if (digitalRead(left) = LOW & button_position > 0){
     button_position = button_position - 1;
-    setRow(0, 7, B00000000);
   }
    
   if (digitalRead(right) = LOW & button_position < 7){
     button_position = button_position + 1;
-    setRow(0, 7, B00000000);
   }
 
   setLed(0, 7, button_position, true);
 }
 
 void setup() {
-  
-  pinMode(left,INPUT_PULLUP);
-  pinMode(right,INPUT_PULLUP);
-
-  Serial.begin(9600);
-  
-  lc.shutdown(0, false);
-  lc.setIntensity(0, 8);
-  lc.clearDisplay(0);
-  setLed(0, 7, button_position, true);
+    Serial.begin(9600);
 
 }
 
